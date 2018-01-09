692f063d3914d102d46853f0b756bfae359072f9
Foyzul Karim <foyzulkarim@gmail.com>
2018-01-09 21:46:21 +0600

    course saved successfully

--- projects/Lms/LmsWeb/Controllers/CourseController.cs
+++ projects/Lms/LmsWeb/Controllers/CourseController.cs
@@ -28,16 +28,7 @@ namespace LmsWeb.Controllers
     public class CourseController : BaseController<Course, CourseRequestModel, CourseViewModel>
     { 
     
-        //[HttpPost]
-        //[Route("Add")]
-        //[ActionName("Add")]
-        //public IHttpActionResult Post(Course course)
-        //{
-        //    var id = Guid.NewGuid() + "\t" + DateTime.Now + " " + course.Name;
-        //    return this.Ok($"{id}");
-        //}
-
-        public CourseController()
+       public CourseController()
             : base(new BaseService<Course, CourseRequestModel, CourseViewModel>(new BaseRepository<Course>(BusinessDbContext.Create())))
         {
         }
